323abe2c5d81a91a88f2f30b9c804aa32198ad66
riazul islam <riazul.codes@gmail.com>
2026-01-12 23:27:49 +0600

    feat: customized button and input components

---
+++ src/theme/components/input/Select.tsx
@@ -0,0 +1,26 @@
+import type { Theme, Components } from '@mui/material/styles';
+
+const Select: Components<Omit<Theme, 'components'>>['MuiSelect'] = {
+  styleOverrides: {
+    root: ({ theme }) => ({
+      padding: 0,
+      '&.MuiInputBase-root': {
+        backgroundColor: 'transparent !important',
+      },
+      '& .MuiBox-root': {
+        fontSize: theme.typography.h4.fontSize,
+      },
+    }),
+    select: ({ theme }) => ({
+      padding: theme.spacing(1),
+      paddingLeft: theme.spacing(1.25),
+      paddingRight: '0 !important',
+      backgroundColor: 'transparent !important',
+      fontSize: theme.typography.caption.fontSize,
+      color: theme.palette.text.primary,
+      fontWeight: 600,
+    }),
+  },
+};
+
+export default Select;
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,6 +3,20 @@ import customShadows from './shadows';
 import typography from './typography';
 import palette from './palette';
 
+import Button from './components/button/Button';
+import ButtonBase from './components/button/ButtonBase';
+import IconButton from './components/button/IconButton';
+import Toolbar from './components/button/Toolbar';
+
+import InputBase from './components/input/InputBase';
+import Checkbox from './components/input/Checkbox';
+import Select from './components/input/Select';
+import InputAdornment from './components/input/InputAdornment';
+import FilledInput from './components/input/FilledInput';
+import FormControlLabel from './components/input/FormControlLabel';
+import OutlinedInput from './components/input/OutlinedInput';
+import Slider from './components/input/Slider';
+
 import CssBaseline from './components/utils/CssBaseline';
 
 export const theme = createTheme({
@@ -10,6 +24,20 @@ export const theme = createTheme({
   palette,
   customShadows,
   components: {
+    MuiButton: Button,
+    MuiButtonBase: ButtonBase,
+    MuiIconButton: IconButton,
+    MuiToolbar: Toolbar,
+
+    MuiInputBase: InputBase,
+    MuiFilledInput: FilledInput,
+    MuiOutlinedInput: OutlinedInput,
+    MuiInputAdornment: InputAdornment,
+    MuiFormControlLabel: FormControlLabel,
+    MuiCheckbox: Checkbox,
+    MuiSelect: Select,
+    MuiSlider: Slider,
+
     MuiCssBaseline: CssBaseline,
   },
 });
