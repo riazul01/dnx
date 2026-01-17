b5416744f4bc9d947a5f35bea664612431630d21
riazul islam <riazul.codes@gmail.com>
2026-01-17 20:58:24 +0600

    feat: add week calendar and task today section

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,39 @@
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Footer from 'components/common/Footer';
+import TaskToday from 'components/sections/dashboard/TaskToday';
+import WeekCalendar from 'components/sections/dashboard/WeekCalendar';
+
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <Stack direction={{ xs: 'column', md: 'row' }}>
+      <Stack p={3.5} spacing={3.5} direction="column" width={{ xs: 1, md: 'calc(100% - 460px)' }}>
+        <h1>Hello</h1>
+        <Footer />
+      </Stack>
+
+      <Box
+        width={{ xs: 1, md: 460 }}
+        height={{ xs: 'auto', md: 'calc(100vh - 90px)' }}
+        overflow="scroll"
+        bgcolor="info.main"
+        position="sticky"
+        top={90}
+        sx={{
+          '&:hover, &:focus': {
+            '&::-webkit-scrollbar-thumb': {
+              visibility: 'visible',
+            },
+          },
+        }}
+      >
+        <Stack direction="column" p={3.5} spacing={3.5} width={1}>
+          <WeekCalendar />
+          <TaskToday />
+        </Stack>
+      </Box>
+    </Stack>
+  );
 };
 
 export default Dashboard;
