a33ac6672f9424f93593b4c365ec53b10fc29192
riazul islam <riazul.codes@gmail.com>
2026-05-03 22:21:04 +0600

    feat: add error404 page and favicon

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,12 +1,13 @@
 import { Suspense, lazy } from 'react';
 import { Outlet, createBrowserRouter } from 'react-router';
+import paths, { rootPaths } from './paths';
 import MainLayout from 'layouts/main-layout';
+import AuthLayout from 'layouts/auth-layout';
 import Splash from 'components/loader/Splash';
 import PageLoader from 'components/loader/PageLoader';
-import AuthLayout from 'layouts/auth-layout';
-import paths, { rootPaths } from './paths';
 import Signin from 'pages/authentication/Signin';
 import Signup from 'pages/authentication/Signup';
+import Error404 from 'pages/Error404';
 
 const App = lazy(() => import('App'));
 const Dashboard = lazy(() => import('pages/Dashboard'));
@@ -53,6 +54,10 @@ const router = createBrowserRouter(
               element: <Signup />,
             },
           ],
+        },
+        {
+          path: '*',
+          element: <Error404 />,
         },
       ],
     },
